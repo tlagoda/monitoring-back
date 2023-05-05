8d04a13b6e841d1efc68815646c49d1b39bb11ad
tlagoda <thibautlagoda@hotmail.fr>
2023-05-05 22:08:14 +0200

    add historic.service and getHistoric function

--- src/historic/historic.controller.ts
+++ src/historic/historic.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { HistoricService } from './historic.service';
 
 @Controller('historic')
 export class HistoricController {
   @Get()
-  getHistoric(): string {
-    return 'historic';
+  getHistoric(): any[] {
+    return HistoricService.getHistoric();
   }
 }
--- src/historic/historic.service.ts
+++ src/historic/historic.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HistoricService {}
+export class HistoricService {
+  static getHistoric(): any[] {
+    const historic = [
+      {
+        date: '03/05/2023',
+        series: 3,
+        reps: 20,
+        weight: 8,
+      },
+    ];
+    return historic;
+  }
+}
