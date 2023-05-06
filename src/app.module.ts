ddad16483e016de014a159a090d40520637245ae
tlagoda <thibautlagoda@hotmail.fr>
2023-05-06 10:04:51 +0200

    delete performance.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PerformanceController } from './performance/performance.controller';
 import { PerformanceService } from './performance/performance.service';
-import { PerformanceModule } from './performance/performance.module';
 
 @Module({
-  imports: [PerformanceModule],
   controllers: [AppController, PerformanceController],
   providers: [AppService, PerformanceService],
 })
