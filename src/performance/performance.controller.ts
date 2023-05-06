89b5835fe1a231966bb3c15951ae1102217b58a5
tlagoda <thibautlagoda@hotmail.fr>
2023-05-06 09:39:22 +0200

    wip: use validationPipes

--- src/performance/performance.controller.ts
+++ src/performance/performance.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  ValidationPipe,
+  UsePipes,
+} from '@nestjs/common';
 import { PerformanceService } from './performance.service';
 import { CreatePerformanceDto } from './dto/performance-create.dto';
 
@@ -11,6 +18,7 @@ export class PerformanceController {
     return this.performanceService.getPerformances();
   }
 
+  @UsePipes(new ValidationPipe())
   @Post()
   create(@Body() performance: CreatePerformanceDto) {
     return this.performanceService.create(performance);
