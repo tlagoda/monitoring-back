20a3364164b8ad036f1fcd479f482722238622a7
tlagoda <thibautlagoda@hotmail.fr>
2023-05-11 21:28:55 +0200

    change path to user create dto

--- src/users/dto/user-create.dto.ts
+++ src/users/dto/user-create.dto.ts
@@ -1,5 +1,24 @@
-export class UserCreateDto {
-    readonly username: string;
-    readonly email: string;
-    readonly password: string;
+import {
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  IsString,
+  IsIn,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  readonly password: string;
+
+  @IsIn(['M', 'F'])
+  readonly sexe?: string;
 }
